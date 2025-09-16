1f0e68050af2eab5cbae6ce1ab9cfb0caf8f32af
Neal Denervaud <Neal_Denervaud@sluz.ch>
2025-09-16 16:02:59 +0200

    added task 3 and 4

--- task_1_3.py
+++ task_1_3.py
@@ -3,3 +3,7 @@ Name = input("Gib deinen Namen ein: ")
 print(Name.upper())
 print(Name.lower())
 print(Name[::-1])
+
+Wort1 = input("Gib ein Wort ein: ")
+Wort2 = input("Gib ein zweites Wort ein: ")
+print(Wort1 + Wort2)
---
+++ task_1_4.py
@@ -0,0 +1,7 @@
+zahl1 = float(input("Nenne eine Zahl: "))
+zahl2 = float(input("Nenne eine andere Zahl: "))
+
+print(f"Addition: {zahl1 + zahl2:.2f} ")
+print(f"Subtraktion: {zahl1 - zahl2:.2f} ")
+print(f"Multiplikation : {zahl1 * zahl2:.2f} ")
+print(f"Division: {zahl1 / zahl2:.2f} ")
