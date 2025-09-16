ee59c50b9aaaa2ab766c148b4e97d90271684860
Neal Denervaud <Neal_Denervaud@sluz.ch>
2025-09-16 15:34:22 +0200

    added 2.

--- task_1_1.py
+++ task_1_1.py
@@ -1,4 +1,7 @@
 # Block 1 Aufgabe 1
 
 name = input("Gib deinen Namen ein: ")
+Jahr = int(input("Gib das aktuelle Jahr ein: "))
+Alter = int(input("Gib dein Alter ein: "))
 print(f"Hallo {name}, wie geht es dir heute?")
+print(f"Du bist im Jahr {Jahr - Alter} geboren.")
--- task_1_3.py
+++ task_1_3.py
@@ -1,1 +1,5 @@
 # Block 1 Aufgabe 3
+Name = input("Gib deinen Namen ein: ")
+print(Name.upper())
+print(Name.lower())
+print(Name[::-1])
