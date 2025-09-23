52a0d93587a49bdabab75a78bbc2272230d76467
Neal Denervaud <Neal_Denervaud@sluz.ch>
2025-09-23 15:44:34 +0200

    task 8

---
+++ task_1_8.py
@@ -0,0 +1,6 @@
+zahl = int(input("Nenne mir eine Zahl: "))
+
+if (zahl <20 and zahl >10):
+    print(f"Die Zahl {zahl} befindet sich zwischen 10 und 20!")
+elif (zahl <0 or zahl >100):
+    print(f"Die Zahl {zahl} ist kleiner als 0 oder grösser als 100!")
