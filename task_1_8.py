8115b55f55062b8ccf2ecd0b1b9ba2b4c8070847
Neal Denervaud <Neal_Denervaud@sluz.ch>
2025-09-23 16:17:05 +0200

    Task 9

--- task_1_8.py
+++ task_1_8.py
@@ -4,3 +4,5 @@ if (zahl <20 and zahl >10):
     print(f"Die Zahl {zahl} befindet sich zwischen 10 und 20!")
 elif (zahl <0 or zahl >100):
     print(f"Die Zahl {zahl} ist kleiner als 0 oder grösser als 100!")
+    
+""" Bei'and' müssen alle bedingungen erfüllt sein und bei 'or' muss nur eine der Bedingungen erfüllt sein. """
