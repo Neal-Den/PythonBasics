6f7022f86d89002616b88b4b98c6ca45e4973ea9
Neal Denervaud <Neal_Denervaud@sluz.ch>
2025-09-23 15:37:42 +0200

    task 7

--- task_1_6.py
+++ task_1_6.py
@@ -1,11 +1,7 @@
 riegel = 3.20
 geld = float(input("Wieviel Geld hast du?: "))
-
-# Anzahl der Riegel, die du kaufen kannst (ganzzahlig)
 riegelmenge = int(geld // riegel)
-
-# Restgeld berechnen
 restgeld = geld - (riegelmenge * riegel)
 
 print(f"Du kannst dir {riegelmenge} Riegel kaufen.")
-print(f"Restgeld: {restgeld:.2f} €")
+print(f"Restgeld: {restgeld:.2f} Fr.")
